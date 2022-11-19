8eb611d707fb4ace4f335a1d55d3bb428767bc8c
Gustavo Chiquitin <luis.gustavo09@hotmail.com>
2022-11-18 20:24:01 -0400

    Buscar usuario na API

--- localizadorDePerfil/src/App.tsx
+++ localizadorDePerfil/src/App.tsx
@@ -1,10 +1,43 @@
-import React from "react";
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import React from "react";
 
+type GITHUBResponse = {
+  html_url: string;
+  name: string;
+  avatar_url: string;
+  bio: string;
+};
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [name, setName] = useState("Aguardando...");
+  const [bio, setBio] = useState("Aguardando...");
+  const [perfil, setPerfil] = useState("/");
+  const [avatarURL, setAvatarURL] = useState("./src/assets/avatar.png");
 
-  const fotoPadrao = "https://www.w3schools.com/howto/img_avatar.png";
+  useEffect(() => {
+    setUserName("")
+    setName("Aguardando...")
+    setBio("Aguardando...")
+    setPerfil("/")
+  }, [])
+
+
+  const handleSearch = () => {
+    axios
+      .get<GITHUBResponse>(`https://api.github.com/users/${userName}`)
+      .then((res) => {
+        setName(res.data.name);
+        setBio(res.data.bio);
+        setAvatarURL(res.data.avatar_url);
+        setPerfil(res.data.html_url);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="container-app">
@@ -16,12 +49,17 @@ function App() {
             <h1>Buscador de Perfis do GITHUB</h1>
             <input
               type="text"
-              placeholder="Digite o nome do usuário" />
-            <button>Buscar</button>
+              placeholder="Digite um username"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <button onClick={handleSearch}>Buscar</button>
           </div>
           <div className="content">
             <div>
-              <img src={fotoPadrao} alt="Perfil" />
+              <img src={avatarURL} alt="avatar" />
+              <h1>{name}</h1>
+              <p>{bio}</p>
+              <a className="verPerfil" href={perfil} >Ver perfil</a>
             </div>
           </div>
         </main>
